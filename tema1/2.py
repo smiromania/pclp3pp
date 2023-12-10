6689a49ba63967456ba6db927c98025ceea13cd7
smiromania <tedygamer7@gmail.com>
2023-12-10 14:33:10 +0200

    Integrated Tkinter app for exercise 2

--- tema1/2.py
+++ tema1/2.py
@@ -1,18 +1,77 @@
-import pandas as pd
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
+import pandas as pd
 
+class PlotApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Pulse Graphs")
 
-file_path = 'data.csv'  
+        self.file_path = 'tema1/data.csv'
 
-data = pd.read_csv(file_path)
+        self.data = pd.read_csv(self.file_path)
 
-data.plot(title='Toate Valorile')
-plt.show()
+        self.plot_windows = []
 
-X = 6
-data.head(X).plot(title=f'Primele {X} Valori')
-plt.show()
+        self.create_widgets()
 
-Y = 11
-data[['Durata', 'Puls']].tail(Y).plot(title=f'Ultimele {Y} Valori pentru Durata și Puls')
-plt.show()
+    def create_widgets(self):
+        btn_all_values = ttk.Button(self.master, text="Show all values", command=self.plot_all_columns)
+        btn_all_values.pack(pady=10)
+
+        btn_first_x_values = ttk.Button(self.master, text="Show first values", command=lambda: self.plot_first_x_values(6))
+        btn_first_x_values.pack(pady=10)
+
+        btn_last_y_values = ttk.Button(self.master, text="Show last values", command=lambda: self.plot_last_y_values(11))
+        btn_last_y_values.pack(pady=10)
+
+        btn_reset = ttk.Button(self.master, text="Reset", command=self.reset_plots)
+        btn_reset.pack(pady=10)
+
+    def plot_all_columns(self):
+        self.close_plot_windows()  
+        plt.figure(figsize=(6, 4))
+        for col in self.data.columns:
+            plt.plot(self.data.index, self.data[col], label=col)
+        plt.legend()
+        plt.title('Toate Coloanele')
+        self.show_plot()
+
+    def plot_first_x_values(self, x):
+        self.close_plot_windows()  
+        plt.figure(figsize=(6, 4))
+        for col in self.data.columns:
+            plt.plot(self.data.index[:x], self.data[col][:x], label=col)
+        plt.legend()
+        plt.title(f'Primele {x} Valori')
+        self.show_plot()
+
+    def plot_last_y_values(self, y):
+        self.close_plot_windows() 
+        plt.figure(figsize=(6, 4))
+        for col in ['Durata', 'Puls']:  
+            plt.plot(self.data.index[-y:], self.data[col][-y:], label=col)
+        plt.legend()
+        plt.title(f'Ultimele {y} Valori pentru Durata și Puls')
+        self.show_plot()
+
+    def reset_plots(self):
+        self.close_plot_windows()  
+
+    def show_plot(self):
+        canvas = FigureCanvasTkAgg(plt.gcf(), master=self.master)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.plot_windows.append(canvas.get_tk_widget())
+
+    def close_plot_windows(self):
+        for window in self.plot_windows:
+            window.destroy()
+        self.plot_windows = []
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PlotApp(root)
+    root.mainloop()
